2b63c06c5ae254cd6782fa2045f535857089dc45
Hyojin(Kitty) Kwak <Kitty.Kwak@charlottetilbury.com>
2022-10-22 21:50:16 +0100

    Read and show text file line by line with click

---
+++ Assets/Scripts/TextChanges.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using UnityEngine;
+using TMPro;
+
+public class TextChanges : MonoBehaviour
+{
+    public TextMeshProUGUI ScriptTxt;
+    string[] IntroScript = new string[] { ".....", "Where am I?", "Spider Man" };
+    protected FileInfo theSourceFile = null;
+    protected StreamReader reader = null;
+    protected string text = " "; // assigned to allow first line to be read below
+ 
+    void Start()
+    {
+        ScriptTxt = GetComponent<TextMeshProUGUI>();
+
+        theSourceFile = new FileInfo (Application.dataPath + "/Resources/Scripts" + "/" + "Intro.txt");
+        reader = theSourceFile.OpenText();
+    }
+    
+    // Update is called once per frame
+    void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (text != null) {
+            text = reader.ReadLine();
+            //Console.WriteLine(text);
+            ScriptTxt.text = text;
+
+            print (text);
+           }
+        }
+    }
+}
